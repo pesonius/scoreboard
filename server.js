7abdd87b19cd7639fed57d459486eca4a92d2d91
Mikael Pesonius <mikael.pesonius@bcbmedical.com>
2026-03-21 15:49:32 +0200

    refactor: move files to repo root for GitHub Pages
    - GitHub Pages only supports /(root) or /docs as source folder
    - server.js updated to serve from __dirname instead of public/

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.listen(3000, '0.0.0.0', () => {
   console.log('Scoreboard at http://0.0.0.0:3000');
